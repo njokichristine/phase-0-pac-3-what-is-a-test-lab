4141ec41cea181f212b5b3d2fe0de1ca7555dc98
njokichristine <christinenjoki6900@gmail.com>
2022-12-01 16:12:31 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const name = "Joe";
-const height = 74;
+const height1 = 74;
+const name2 = "Susan";
+const height2 = 39;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message)
